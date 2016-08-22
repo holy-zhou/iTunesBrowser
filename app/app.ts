8ae695d332c6771b36b71e62dcd3aa28b1bbe9ae
Holy Zhou <holy.zhou@gmail.com>
2016-08-23 01:49:11 +0800

    Add Baidu Map

--- app/app.ts
+++ app/app.ts
@@ -6,36 +6,37 @@ import {SearchPage} from './pages/search/search';
 import {SettingsPage} from './pages/setting/setting';
 import {ContactusPage} from './pages/contactus/contactus';
 import {PhotoPage} from './pages/photo/photo';
-
+import {BmapPage} from './pages/bmap/bmap';
 @Component({
-    templateUrl: 'build/app.html',
+  templateUrl: 'build/app.html',
 })
 export class MyApp {
-    @ViewChild(Nav) nav: Nav;
+  @ViewChild(Nav) nav: Nav;
 
-    rootPage: any = SearchPage;
-    pages: Array<{ title: string, component: any }>;
+  rootPage: any = SearchPage;
+  pages: Array<{ title: string, component: any }>;
 
-    constructor(platform: Platform) {
-        platform.ready().then(() => {
-            // Okay, so the platform is ready and our plugins are available.
-            // Here you can do any higher level native things you might need.
-            StatusBar.styleDefault();
-        });
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
 
-        this.pages = [
-            { title: 'Search', component: SearchPage },
-            { title: 'Settings', component: SettingsPage },
-            { title: 'Contact Us', component: ContactusPage },
-            { title: 'Take Picture', component: PhotoPage }
-        ];
-    }
+    this.pages = [
+      { title: 'Search', component: SearchPage },
+      { title: 'Settings', component: SettingsPage },
+      { title: 'Contact Us', component: ContactusPage },
+      { title: 'Take Picture', component: PhotoPage },
+      { title: 'BaiduMap', component: BmapPage }
+    ];
+  }
 
-    openPage(page) {
-        // Reset the content nav to have just this page
-        // we wouldn't want the back button to show in this scenario
-        this.nav.setRoot(page.component);
-    }
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
 }
 
 ionicBootstrap(MyApp);
---
+++ app/pages/bmap/bmap.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {BaiduMap, OfflineOptions, ControlAnchor, NavigationControlType} from 'angular2-baidu-map';
+
+declare var BMap;
+
+@Component({
+  templateUrl: 'build/pages/map/map.html',
+  directives: [BaiduMap]
+})
+export class BmapPage {
+  opts: any;
+  constructor(private navCtrl: NavController) {
+  }
+
+  ionViewLoaded() {
+    this.opts = {
+      center: {
+        longitude: 121.506191,
+        latitude: 31.245554
+      },
+      zoom: 17
+    };
+  }
+
+  loadMap(e: any) {
+    console.log(e); // e here is the instance of BMap.Map
+  }
+
+  clickMarker(marker: any) {
+    console.log('The clicked marker is', marker);
+  }
+
+}
