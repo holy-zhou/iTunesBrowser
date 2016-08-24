679b9a5bccc0c5be1caec59f9b0d93b444c9ca4b
Holy Zhou <holy.zhou@gmail.com>
2016-08-24 17:11:54 +0800

    try extenal Map App

--- app/pages/glocation/glocation.ts
+++ app/pages/glocation/glocation.ts
@@ -1,29 +1,39 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 import {Geolocation} from 'ionic-native';
-/*
-  Generated class for the GlocationPage page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   templateUrl: 'build/pages/glocation/glocation.html',
 })
 export class GlocationPage {
-  Location: string;
-  constructor(private navCtrl: NavController) {
-
+  _isAndroid: boolean;
+  _isiOS: boolean;
+  Location: string = '31.233611,121.4651457,16.08';
+  constructor(private platform: Platform) {
+    this._isAndroid = platform.is('android');
+    this._isiOS = platform.is('ios');
   }
   getLocation() {
     Geolocation.getCurrentPosition({ enableHighAccuracy: true })
       .then((pos) => {
         let lat = pos.coords.latitude;
         let lon = pos.coords.longitude;
-
         this.Location = `${lat},${lon}`;
       }).catch((err) => {
         console.log(err);
       });
+  }
+
+  openMapsApp() {
+    if (this._isiOS) {
+      window.open(`http://maps.apple.com/?q=${Location}_system`);
+      return;
+    }
+    if (this._isAndroid) {
+      window.open('geo:' + Location);
+      return;
+    }
+    window.open(`http://maps.google.com/?q=${Location}_system`);
+    return;
   }
 }
